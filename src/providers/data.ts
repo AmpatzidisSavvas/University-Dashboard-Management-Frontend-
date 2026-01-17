18fa647392aa8e5a597646154148bd1b35145371
AmpatzidisSavvas <savvas.ampa@gmail.com>
2026-01-17 10:56:46 +0200

    Implement fixes

--- src/providers/data.ts
+++ src/providers/data.ts
@@ -1,6 +1,11 @@
 import { BACKEND_BASE_URL } from "@/constants";
 import { ListResponse } from "@/types";
 import { createDataProvider, CreateDataProviderOptions} from "@refinedev/rest";
+import { error } from "console";
+
+if (!BACKEND_BASE_URL) {
+  throw new Error('BACKEND_BASE_URL is not configured. Please set VITE_BACKEND_BASE_URL in your .env file')
+}
 
 const options: CreateDataProviderOptions = {
     getList: {
@@ -32,13 +37,13 @@ const options: CreateDataProviderOptions = {
       },
 
       mapResponse: async (response) => {
-        const payload: ListResponse = await response.json();
+        const payload: ListResponse = await response.clone().json();
 
         return payload.data ?? [];
       },
 
       getTotalCount: async (response) => {
-        const payload: ListResponse = await response.json();
+        const payload: ListResponse = await response.clone().json();
 
         return payload.pagination?.total ?? payload.data?.length ?? 0;
       }
