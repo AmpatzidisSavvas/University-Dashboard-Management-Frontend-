b7104dae8bbeab8ff8cf9513b9d29dc8f36d014e
AmpatzidisSavvas <savvas.ampa@gmail.com>
2026-01-27 11:13:05 +0200

    Implement  filter class list changes

--- src/providers/data.ts
+++ src/providers/data.ts
@@ -1,54 +1,68 @@
-import { BACKEND_BASE_URL } from "@/constants";
-import { CreateResponse, GetOneResponse, ListResponse } from "@/types";
-import { createDataProvider, CreateDataProviderOptions} from "@refinedev/rest";
-import { error } from "console";
+import { createDataProvider, CreateDataProviderOptions } from "@refinedev/rest";
 
-if (!BACKEND_BASE_URL) {
-  throw new Error('BACKEND_BASE_URL is not configured. Please set VITE_BACKEND_BASE_URL in your .env file')
-}
+import { CreateResponse, GetOneResponse, ListResponse } from "@/types";
+import { BACKEND_BASE_URL } from "@/constants";
 
 const options: CreateDataProviderOptions = {
-    getList: {
-      getEndpoint: ({ resource }) => resource,
+  getList: {
+    getEndpoint: ({ resource }) => resource,
 
-      buildQueryParams: async ({ resource, pagination, filters}) => {
+    buildQueryParams: async ({ resource, pagination, filters }) => {
+      const params: Record<string, string | number> = {};
+
+      if (pagination?.mode !== "off") {
         const page = pagination?.currentPage ?? 1;
         const pageSize = pagination?.pageSize ?? 10;
 
-        const params: Record<string, string|number> = { page, pageSize};
-
-        filters?.forEach((filter) => {
-
-          const field = 'field' in filter ? filter.field : '';
-
-          const value = String(filter.value);
-
-          if (resource === 'subjects') {
-            if (field === 'deparment') {
-              params.deparment = value;
-            }
-            if (field === 'name' || field=== 'code') {
-              params.search = value;
-            }
-          }
-        })
-
-        return params;
-      },
-
-      mapResponse: async (response) => {
-        const payload: ListResponse = await response.clone().json();
-
-        return payload.data ?? [];
-      },
-
-      getTotalCount: async (response) => {
-        const payload: ListResponse = await response.clone().json();
-
-        return payload.pagination?.total ?? payload.data?.length ?? 0;
+        params.page = page;
+        params.limit = pageSize;
       }
+
+      filters?.forEach((filter) => {
+        const field = "field" in filter ? filter.field : "";
+        const value = String(filter.value);
+
+        if (field === "role") {
+          params.role = value;
+        }
+
+        if (resource === "departments") {
+          if (field === "name" || field === "code") params.search = value;
+        }
+
+        if (resource === "users") {
+          if (field === "search" || field === "name" || field === "email") {
+            params.search = value;
+          }
+        }
+
+        if (resource === "subjects") {
+          if (field === "department") params.department = value;
+          if (field === "name" || field === "code") params.search = value;
+        }
+
+        if (resource === "classes") {
+          if (field === "name") params.search = value;
+          if (field === "subject") params.subject = value;
+          if (field === "teacher") params.teacher = value;
+        }
+      });
+
+      return params;
     },
-    create: {
+
+    mapResponse: async (response) => {
+      const payload: ListResponse = await response.json();
+      return payload.data ?? [];
+    },
+
+    getTotalCount: async (response) => {
+      const payload: ListResponse = await response.json();
+      return payload.pagination?.total ?? payload.data?.length ?? 0;
+    },
+  },
+
+  create: {
     getEndpoint: ({ resource }) => resource,
 
     buildBodyParams: async ({ variables }) => variables,
@@ -67,11 +81,8 @@ const options: CreateDataProviderOptions = {
       return json.data ?? {};
     },
   },
+};
 
-}
+const { dataProvider } = createDataProvider(BACKEND_BASE_URL, options);
 
-
-
-const { dataProvider} = createDataProvider(BACKEND_BASE_URL, options);
-
-export { dataProvider};
+export { dataProvider };
